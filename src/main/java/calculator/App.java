4c48ad709727d207d9948c320d35684f54149c7a
kang <m0nsia67@gmail.com>
2024-07-24 19:44:46 +0900

    Level_1_4

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -5,45 +5,59 @@ public class App {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        double result;
 
-        System.out.print("첫 번째 숫자를 입력하세요: ");
-        int num1 = sc.nextInt();
-        System.out.print("두 번째 숫자를 입력하세요: ");
-        int num2 = sc.nextInt();
+            while(true){
+                // 첫 번째 숫자 입력 받기
+                System.out.println("첫 번째 숫자를 입력하세요: ");
+                double num1 = sc.nextDouble();
+                System.out.println("숫자를 입력해주세요.");
 
 
-        System.out.println("사칙연산 기호를 입력하세요.");
-        /*chatAt(idx) : charAt 메서드는 매개변수로 char 타입으로 변환 하고자 하는 문자열의 위치를 받는다.*/
-        char operator = sc.next().charAt(0);
-        int result = 0;
+                System.out.print("두 번째 숫자를 입력하세요: ");
+                double num2 = sc.nextInt();
+                //사칙연산
+                System.out.print("사칙연산 기호를 입력하세요: ");
+                char operator = sc.next().charAt(0);
 
-        switch (operator) {
-            case '+':
-                result = num1 + num2;
-                System.out.println("결과: " + num1 + "+" + num2 + "=" + result);
-                break;
-            case '-':
-                result = num1 - num2;
-                System.out.println("결과: " + num1 + "-" + num2 + "=" + result);
+                // 계산
+                switch (operator) {
+                    case '+':
+                        result = num1 + num2;
+                        System.out.println("결과: " + num1 + "+" + num2 + "=" + result);
+                        break;
 
-                break;
-            case '*':
-                result = num1 * num2;
-                System.out.println("결과: " + num1 + "*" + num2 + "=" + result);
+                    case '-':
+                        result = num1 - num2;
+                        System.out.println("결과: " + num1 + "-" + num2 + "=" + result);
+                        break;
 
-                break;
-            case '/':
-                if (num2 != 0) {
-                    result = num1 / num2;
-                    System.out.println("결과: " + num1 + "/" + num2 + "=" + result);
+                    case '*':
+                        result = num1 * num2;
+                        System.out.println("결과: " + num1 + "*" + num2 + "=" + result);
+                        break;
 
-                } else {
-                    System.out.println("0으로 나눌 수 없습니다.");
+                    case '/':
+                        if (num2 != 0) {
+                            result = num1 / num2;
+                            System.out.println("결과: " + num1 + "/" + num2 + "=" + result);
+                        } else {
+                            System.out.println("0으로 나눌 수 없습니다.");
+                        }
+                        break;
+                    default:
+                        System.out.println("잘못된 연산자 =입니다.");
+                        continue;
                 }
-                break;
-            default:
-                System.out.println("다시 입력해주세요.");
-        }
+                // 계산을 계속할지 안할지
+                System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
+
+                String check = sc.next();
+                if (check.equals("exit")) {
+                    break;
+                }
+            }
+
 
     }
 }
