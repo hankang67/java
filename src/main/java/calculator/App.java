7d7a12564eb33a466cf7eef74458ab8dae69a8bf
kang <m0nsia67@gmail.com>
2024-07-29 13:43:15 +0900

    Level_1_5

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -5,60 +5,62 @@ public class App {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        double result;
+        double result = 0;
+        double[] save = new double[10]; // 결과를 저장할 배열
+        int count = 0;
 
-            while(true){
-                // 첫 번째 숫자 입력 받기
-                System.out.println("첫 번째 숫자를 입력하세요: ");
-                double num1 = sc.nextDouble();
-                System.out.println("숫자를 입력해주세요.");
+        while (true) {
+            // 첫 번째 숫자 입력 받기
+            System.out.print("첫 번째 숫자를 입력하세요: ");
+            double num1 = sc.nextDouble();
 
+            System.out.print("두 번째 숫자를 입력하세요: ");
+            double num2 = sc.nextDouble();
+            //사칙연산
+            System.out.print("사칙연산 기호를 입력하세요: ");
+            char operator = sc.next().charAt(0);
 
-                System.out.print("두 번째 숫자를 입력하세요: ");
-                double num2 = sc.nextInt();
-                //사칙연산
-                System.out.print("사칙연산 기호를 입력하세요: ");
-                char operator = sc.next().charAt(0);
-
-                // 계산
-                switch (operator) {
-                    case '+':
-                        result = num1 + num2;
-                        System.out.println("결과: " + num1 + "+" + num2 + "=" + result);
-                        break;
-
-                    case '-':
-                        result = num1 - num2;
-                        System.out.println("결과: " + num1 + "-" + num2 + "=" + result);
-                        break;
-
-                    case '*':
-                        result = num1 * num2;
-                        System.out.println("결과: " + num1 + "*" + num2 + "=" + result);
-                        break;
-
-                    case '/':
-                        if (num2 != 0) {
-                            result = num1 / num2;
-                            System.out.println("결과: " + num1 + "/" + num2 + "=" + result);
-                        } else {
-                            System.out.println("0으로 나눌 수 없습니다.");
-                        }
-                        break;
-                    default:
-                        System.out.println("잘못된 연산자 =입니다.");
-                        continue;
+            // 계산
+            if (operator == '+') {
+                result = num1 + num2;
+                System.out.println("결과: " + num1 + "+" + num2 + "=" + result);
+            } else if (operator == '-') {
+                result = num1 - num2;
+                System.out.println("결과: " + num1 + "-" + num2 + "=" + result);
+            } else if (operator == '*') {
+                result = num1 * num2;
+                System.out.println("결과: " + num1 + "*" + num2 + "=" + result);
+            } else if (operator == '/') {
+                if (num2 != 0) {
+                    result = num1 / num2;
+                    System.out.println("결과: " + num1 + "/" + num2 + "=" + result);
+                } else {
+                    System.out.println("0으로 나눌 수 없습니다.");
                 }
-                // 계산을 계속할지 안할지
-                System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
-
-                String check = sc.next();
-                if (check.equals("exit")) {
-                    break;
-                }
+            } else {
+                System.out.println("잘못된 연산자입니다.");
+                continue;
+            }
+            // 1-5  결과 저장
+            if (count < save.length) {
+                save[count] = result;
+                count++;
+            } else {
+                System.out.println("결과 저장 공간이 가득 찼습니다.");
             }
 
+            // 계산을 계속할지 안할지
+            System.out.print("더 계산하려면 아무문자나 입력하세요. (exit 입력 시 종료)");
 
+            String check = sc.next();
+            if (check.equals("exit")) {
+                break;
+            }
+        }
+        //결과 출력
+        System.out.println("Save:");
+        for (int i =0 ;i<count;i++){
+            System.out.println((i + 1) + ": " + save[i]);
+        }
     }
 }
-
